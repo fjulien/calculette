1a87ac7fa09c44dcceabf6bf890567f2240f1c7d
fjulien <fettingerjulien@yahoo.fr>
2018-11-03 18:30:36 +0100

    Updates

---
+++ precache-manifest.1f096b9bcdbf5fdae03161591f49fb9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9cc27b15e4cd5895685",
+    "url": "/Calculette/static/js/runtime~main.c9cc27b1.js"
+  },
+  {
+    "revision": "52d8c4caff330bbeffa1",
+    "url": "/Calculette/static/js/main.52d8c4ca.chunk.js"
+  },
+  {
+    "revision": "c9761944ad943a8f5482",
+    "url": "/Calculette/static/js/1.c9761944.chunk.js"
+  },
+  {
+    "revision": "52d8c4caff330bbeffa1",
+    "url": "/Calculette/static/css/main.7d3c9494.chunk.css"
+  },
+  {
+    "revision": "c9761944ad943a8f5482",
+    "url": "/Calculette/static/css/1.e4655020.chunk.css"
+  },
+  {
+    "revision": "18ddef956df3c8c17307d7542b35a922",
+    "url": "/Calculette/index.html"
+  }
+];
